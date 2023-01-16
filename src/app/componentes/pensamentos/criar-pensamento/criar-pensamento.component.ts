09d32a5d24a2ce5ddbcb8019bc60f9bebe2cc4b9
Felipe Araujo de Lima <2981585@CABO.IFPE>
2023-01-16 15:01:51 -0300

    validation form implementation

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -23,17 +23,21 @@ export class CriarPensamentoComponent {
     private router: Router,
     private formBuilder: FormBuilder
   ) {
+
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulario reatio'],
-      autoria: [''],
-      modelo: ['modelo2']
-    })
+      conteudo: ['', Validators.compose([Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      modelo: ['modelo1'],
+    });
+
   }
 
   criarPensamento() {
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento']);
-    });
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
   }
 
   cancelar() {
